da63c29bd0aea35ac26b202293527c1db472ac1b
DukareKamini <154221684+DukareKamini@users.noreply.github.com>
2025-09-09 21:37:42 +0530

    Update Que3.py

---
+++ Core_Python/Assignments/Assignment6.py/Que3.py
@@ -0,0 +1,13 @@
+n = int(input('Enter a number :'))
+
+for i in range(n):
+    for j in range(1, n - i):
+        print('', end = ' ')
+
+    num = 1
+    for j in range(i + 1):
+        print(num, end = ' ')
+        num = num * (i - j) // (j + 1)
+
+    print()
+
