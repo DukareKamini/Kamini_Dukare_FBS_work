95b813efcd416dfec56f710862a635713e02f16c
DukareKamini <154221684+DukareKamini@users.noreply.github.com>
2025-09-10 00:21:06 +0530

    Update Que2.py

--- Core_Python/Tests/Test3.py/Que2.py
+++ Core_Python/Tests/Test3.py/Que2.py
@@ -1,2 +1,10 @@
-n = int(input('enter the number :'))
+n = int(input('Enter the Value of n :'))
+sum = 0
+fact = 1
+
+for i in range(1, n + 1):
+    fact = fact * i
+    sum = sum + (i / fact)
+
+print(f'The sum of the series 1/1!+2/2!+3/3!+...+,{n}/{n}! is : {sum}')
 
